706c2c0b28f2fdab06d12363b9ed549eb3042af5
Franco Garza Mendirichaga <francogarza98@gmail.coma>
2021-05-23 21:17:14 -0500

    more progress on browse and match

--- Woofer/Woofer/ViewControllers/HomeViewController.swift
+++ Woofer/Woofer/ViewControllers/HomeViewController.swift
@@ -13,13 +13,29 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var username: UILabel!
     @IBOutlet weak var dogId: UILabel!
+    @IBOutlet weak var bt_dislike: UIButton!
+    @IBOutlet weak var bt_like: UIButton!
+    @IBOutlet weak var bt_viewProfile: UIButton!
     var petsData: [[String:Any]] = [[:]]
     var i = 0
     
+    @IBOutlet weak var currentUsername: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         loadPet()
+        
+        
+        let ref = Database.database().reference(withPath: "users")
+        ref.queryOrdered(byChild: "uid").queryEqual(toValue: Auth.auth().currentUser?.uid).observeSingleEvent(of: .value, with: {snapshot in
+            
+            for child in snapshot.children{
+                let snap = child as! DataSnapshot
+                self.currentUsername.text = snap.key
+                
+            }
+            
+        })
         
         
     }
@@ -27,11 +43,30 @@ class HomeViewController: UIViewController {
     @IBAction func bt_like(_ sender: UIButton) {
         
         db.child("users/\(username.text!)/pets/\(dogId.text!)/status/\(Auth.auth().currentUser!.uid)").setValue("like")
+        db.child("users/\(currentUsername.text!)/likes/\(username.text!)").setValue("like")
+        
+        disableButtons()
+        loadPet()
+        
+    }
+    
+    @IBAction func bt_dislike(_ sender: Any) {
+        
+        
+        db.child("users/\(username.text!)/pets/\(dogId.text!)/status/\(Auth.auth().currentUser!.uid)").setValue("dislike")
+        
+        disableButtons()
+        loadPet()
         
     }
     
     
+    
     func loadPet(){
+        name.text = "name"
+        username.text = "username"
+        dogId.text = "dogId"
+        
         // get the reference for the users
         let ref = Database.database().reference(withPath: "users")
         // get the snapshot of these reference
@@ -50,30 +85,42 @@ class HomeViewController: UIViewController {
                     let snapPets = childPets as! DataSnapshot
                     // create a constant to cast snapPets as a dictionary
                     let petDic = snapPets.value as! [String: Any]
+                    // create a constant to cast status for the pet
                     let status = petDic["status"] as! [String: Any]
-                    
-                    if let contains = status[Auth.auth().currentUser!.uid]{
-                        // do nothing
-                    }else {
-                        if petDic["ownerUID"] as? String == Auth.auth().currentUser?.uid{
-                            
-                        }else{
+                    // check to see if the pet has been given a status by the user before
+                    if status[Auth.auth().currentUser!.uid] == nil{
+                        
+                        if petDic["ownerUID"] as? String != Auth.auth().currentUser?.uid{
                             self.name.text = petDic["name"] as? String
-                            self.username.text = petDic["owner"] as? String
+                            self.username.text = petDic["ownerUID"] as? String
                             self.dogId.text = snapPets.key
+                            self.enableButtons()
                             return
                         }
+                        
                     }
                     
-                    // copy the dictionary to the array of dictionaries we created to store all pets data
-                    self.petsData.append(petDic)
-                    // print test
-                    print(self.petsData[self.i])
-                    // increment iterator
-                    self.i += 1
                 }
             }
         })
+    }
+    
+    func disableButtons(){
+        bt_dislike.alpha = 0.25
+        bt_dislike.isUserInteractionEnabled = false
+        bt_like.alpha = 0.25
+        bt_like.isUserInteractionEnabled = false
+        bt_viewProfile.alpha = 0.25
+        bt_viewProfile.isUserInteractionEnabled = false
+    }
+    
+    func enableButtons(){
+        bt_dislike.alpha = 1
+        bt_dislike.isUserInteractionEnabled = true
+        bt_like.alpha = 1
+        bt_like.isUserInteractionEnabled = true
+        bt_viewProfile.alpha = 1
+        bt_viewProfile.isUserInteractionEnabled = true
     }
     
 }
