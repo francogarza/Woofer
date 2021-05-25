5a8f2a18300130db6fe43e29a8a213d53b8dd431
Franco Garza Mendirichaga <francogarza98@gmail.coma>
2021-05-25 16:15:05 -0500

    test remove folders

--- Woofer/DerivedData/Woofer/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/DLRadioButton.build/DerivedSources/DLRadioButton_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char DLRadioButtonVersionString[];
- extern const double DLRadioButtonVersionNumber;
-
- const unsigned char DLRadioButtonVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:DLRadioButton  PROJECT:Pods-1" "\n";
- const double DLRadioButtonVersionNumber __attribute__ ((used)) = (double)1.;
